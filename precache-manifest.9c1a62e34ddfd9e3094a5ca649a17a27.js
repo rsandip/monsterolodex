2add6dc83d95665175e0bb31dafec860fef1e754
rsandip <sandurijal03@gmail.com>
2019-12-16 02:32:20 +0545

    Updates

---
+++ precache-manifest.9c1a62e34ddfd9e3094a5ca649a17a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5d45eb0a8ba8f2cea115c140ed1aeb",
+    "url": "/monsterolodex/index.html"
+  },
+  {
+    "revision": "a27f856654b76e6af935",
+    "url": "/monsterolodex/static/css/main.8ffa27eb.chunk.css"
+  },
+  {
+    "revision": "eb92fa0cc8e7b299ecdc",
+    "url": "/monsterolodex/static/js/2.0b222406.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterolodex/static/js/2.0b222406.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a27f856654b76e6af935",
+    "url": "/monsterolodex/static/js/main.919e8ce6.chunk.js"
+  },
+  {
+    "revision": "70e09d5dcc5b7d49b174",
+    "url": "/monsterolodex/static/js/runtime-main.057253e5.js"
+  }
+]);
